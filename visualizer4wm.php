6b9fd1f096bb61da6246b15e74f418cec6f32b33
alfred.maghi <almaghi@users.noreply.github.com>
2010-07-18 03:38:53 +0000

    edit html

--- visualizer4wm.php
+++ visualizer4wm.php
@@ -499,7 +499,7 @@ function printHTML($p_javaScriptCode="",
     <h3>Data Visualizer</h3>
     $p_htmlCode
     <div id="documentation">
-    	<tt>{{<a href="http://meta.wikipedia.org/wiki/visualizer4wm">Visualizer</a>}}</tt> &nbsp;
+    	<tt>{{<a href="./index.html">Visualizer</a>}}</tt> &nbsp;
       &#8734; &nbsp; The data visualizer tool is kindly served to you by the <span class="hidden"><a href="http://toolserver.org/">Wikimedia Toolserver</a>.
       	It uses the <a href="http://mediawiki.org/wiki/API">MediaWiki</a>
       	and the <a href="http://code.google.com/intl/fr-FR/apis/visualization/interactive_charts.html">Vizualisation</a></span> APIs.
